1921c37861515feb537cfd04e2c484bd51c611b8
Dmitry <naumenko.dmitry19@gmail.com>
2019-05-19 12:21:59 +0300

    Authorization + started UI (Index + BoardList)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './component/index/index.component';
+import { TokenComponent } from './component/token/token.component';
+import {BoardlistComponent} from './component/boardlist/boardlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: IndexComponent
+  },
+  {
+    path: 'token',
+    component: TokenComponent
+  },
+  {
+    path: 'boards',
+    component: BoardlistComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AppMaterialModule } from './app-material.module';
 import { AppComponent } from './app.component';
+import { BoardlistComponent } from './component/boardlist/boardlist.component';
+import { ListlistComponent } from './component/listlist/listlist.component';
+import { CardlistComponent } from './component/cardlist/cardlist.component';
+import { MemberlistComponent } from './component/memberlist/memberlist.component';
+import { MemberviewComponent } from './component/memberview/memberview.component';
+import { AuthorizeComponent } from './component/authorize/authorize.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { IndexComponent } from './component/index/index.component';
+import { TokenComponent } from './component/token/token.component';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardlistComponent,
+    ListlistComponent,
+    CardlistComponent,
+    MemberlistComponent,
+    MemberviewComponent,
+    AuthorizeComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    TokenComponent
   ],
   imports: [
+    AppRoutingModule,
+    AppMaterialModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
